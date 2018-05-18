45a9be96f18d58796a694f8103d1a2e88c13b343
Mateusz Skalski <mat.skalski@gmail.com>
2018-05-18 19:29:39 +0200

    Utilize webpack with material components

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const glob = require('glob');
+// console.log(path.normalize());
+
+module.exports = [{
+    entry: './src/stylesheet.scss',
+    output: {
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
+        filename: 'style-bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'assets/stylesheet.css',
+                    },
+                },
+                {loader: 'extract-loader'},
+                {loader: 'css-loader'},
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        includePaths: glob.sync('node_modules').map(function (d) {
+                            return path.join(__dirname, d)
+                        })
+                    }
+                },
+            ]
+        }]
+    },
+},
+    {
+        entry: './src/scripts/use-material.js',
+        output: {
+            path: path.resolve(__dirname, 'assets'),
+            filename: 'use-material.js'
+        }
+    }]
+;
